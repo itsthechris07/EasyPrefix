b2e36e616a822989f184b43e25bd7f1341fda8f2
Chris3105x <christian.haas37@gmail.com>
2020-07-19 22:13:14 +0200

    / removes strange symbols in older versions

--- src/com/christian34/easyprefix/messages/Messages.java
+++ src/com/christian34/easyprefix/messages/Messages.java
@@ -3,6 +3,7 @@ package com.christian34.easyprefix.messages;
 import com.christian34.easyprefix.EasyPrefix;
 import com.christian34.easyprefix.files.ConfigData;
 import com.christian34.easyprefix.user.User;
+import com.christian34.easyprefix.utils.VersionController;
 import com.tchristofferson.configupdater.ConfigUpdater;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,7 +11,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,8 +23,13 @@ import java.util.List;
  */
 public class Messages {
     private final static List<String> LANGUAGES = Arrays.asList("en_EN", "de_DE", "it_IT");
+    private static final EasyPrefix instance;
     private static FileConfiguration data;
     private static String language;
+
+    static {
+        instance = EasyPrefix.getInstance();
+    }
 
     public static String getLanguage() {
         return language;
@@ -32,7 +38,7 @@ public class Messages {
     public static void setLanguage(String lang) {
         if (LANGUAGES.contains(lang)) {
             language = lang;
-            EasyPrefix.getInstance().getFileManager().getConfig().set(ConfigData.ConfigKeys.LANG.toString(), lang);
+            instance.getFileManager().getConfig().set(ConfigData.ConfigKeys.LANG.toString(), lang);
             load();
         }
     }
@@ -49,25 +55,56 @@ public class Messages {
     }
 
     public static void load() {
-        try {
-            Plugin plugin = EasyPrefix.getInstance().getPlugin();
-            ConfigData config = EasyPrefix.getInstance().getFileManager().getConfig();
-            language = config.getData().getString("config.lang");
-            String path = "plugins/EasyPrefix";
-            if (!LANGUAGES.contains(language)) {
-                setLanguage("en_EN");
-            }
-            File file = new File(path, language + ".yml");
-            if (!file.exists()) {
-                plugin.saveResource(language + ".yml", false);
-            } else {
-                ConfigUpdater.update(EasyPrefix.getInstance(), language + ".yml", file, new ArrayList<>());
-            }
-            data = YamlConfiguration.loadConfiguration(file);
-            data.save(file);
-        } catch (Exception ignored) {
+        Plugin plugin = instance.getPlugin();
+        ConfigData config = instance.getFileManager().getConfig();
+        language = config.getData().getString("config.lang");
+        String path = "plugins/EasyPrefix";
+        if (!LANGUAGES.contains(language)) {
+            Messages.log("§cCouldn't load messages for language '" + language + "'! Please use a valid language!");
             setLanguage("en_EN");
+            Messages.log("§cYour language has been set to en_EN!");
         }
+        File file = new File(path, language + ".yml");
+        if (!file.exists()) {
+            plugin.saveResource(language + ".yml", false);
+        } else {
+            try {
+                ConfigUpdater.update(instance, language + ".yml", file, new ArrayList<>());
+            } catch (IOException e) {
+                Messages.log("§cCouldn't update file. Please report this error on github.com!");
+                e.printStackTrace();
+            }
+        }
+        if (VersionController.getMinorVersion() == 8) {
+            System.out.print("111");
+            try {
+                replaceInFile(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        data = YamlConfiguration.loadConfiguration(file);
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void replaceInFile(File file) throws IOException {
+        File tempFile = new File("plugins/EasyPrefix", "messages.tmp");
+        tempFile.createNewFile();
+        FileWriter writer = new FileWriter(tempFile);
+
+        Reader reader = new FileReader(file);
+        BufferedReader br = new BufferedReader(reader);
+
+        while (br.ready()) {
+            writer.write(br.readLine().replaceAll("[^\\x20-\\x7e]", "") + "\n");
+        }
+
+        writer.close();
+        br.close();
+        reader.close();
+        file.delete();
+        tempFile.renameTo(file);
     }
 
     public static List<String> getList(Message message) {
