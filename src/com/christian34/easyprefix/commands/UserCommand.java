10c458d1cf3d19a8d71ca0b9c118252ba6e73acd
chris3105x <christian.haas@outlook.at>
2020-08-01 08:43:44 +0200

    + auto completion for command user

--- src/com/christian34/easyprefix/commands/UserCommand.java
+++ src/com/christian34/easyprefix/commands/UserCommand.java
@@ -109,13 +109,11 @@ public class UserCommand implements Subcommand {
         sender.sendMessage(" \n§7--------------=== §5§l" + targetUser.getPlayer().getName() + " §7===--------------\n ");
         sender.sendMessage("§5Group§f: §7" + targetUser.getGroup().getName());
         sender.sendMessage("§5Subgroup§f: §7" + subgroup);
-        sender.sendMessage("§5Prefix§f: §8«§7" + targetUser.getPrefix().replace("§", "&") + "§8»"
-                + (targetUser.hasCustomPrefix() ? " §7(§5customized§7)" : ""));
+        sender.sendMessage("§5Prefix§f: §8«§7" + targetUser.getPrefix().replace("§", "&") + "§8»" + (targetUser.hasCustomPrefix() ? " §7(§5customized§7)" : ""));
         if (targetUser.hasCustomPrefix()) {
             sender.sendMessage(" §7↳ §5last update§f: §7" + new Timestamp(targetUser.getLastPrefixUpdate()).toString());
         }
-        sender.sendMessage("§5Suffix§f: §8«§7" + targetUser.getSuffix().replace("§", "&") + "§8»"
-                + (targetUser.hasCustomSuffix() ? " §7(§5customized§7)" : ""));
+        sender.sendMessage("§5Suffix§f: §8«§7" + targetUser.getSuffix().replace("§", "&") + "§8»" + (targetUser.hasCustomSuffix() ? " §7(§5customized§7)" : ""));
         if (targetUser.hasCustomSuffix()) {
             sender.sendMessage(" §7↳ §5last update§f: §7" + new Timestamp(targetUser.getLastSuffixUpdate()).toString());
         }
@@ -164,6 +162,22 @@ public class UserCommand implements Subcommand {
                 }
                 return complete;
             }
+        } else if (args.size() == 4) {
+            List<String> matches = new ArrayList<>();
+            if (args.get(2).equalsIgnoreCase("setgroup")) {
+                for (Group group : this.instance.getGroupHandler().getGroups()) {
+                    matches.add(group.getName());
+                }
+            } else if (args.get(2).equalsIgnoreCase("setsubgroup")) {
+                for (Subgroup group : this.instance.getGroupHandler().getSubgroups()) {
+                    matches.add(group.getName());
+                }
+            } else if (args.get(2).equalsIgnoreCase("setgender")) {
+                for (GenderType gender : this.instance.getGroupHandler().getGenderTypes()) {
+                    matches.add(gender.getName());
+                }
+            }
+            return matches;
         }
         return Collections.emptyList();
     }
