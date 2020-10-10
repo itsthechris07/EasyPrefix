1bdb2cf9b2062806e88c2032fbd75abe3a0e76ea
chris3105x <christian.haas@outlook.at>
2020-10-10 14:57:31 +0200

    - removed sounds on join or quit

--- src/main/java/com/christian34/easyprefix/listeners/JoinListener.java
+++ src/main/java/com/christian34/easyprefix/listeners/JoinListener.java
@@ -1,7 +1,6 @@
 package com.christian34.easyprefix.listeners;
 
 import com.christian34.easyprefix.EasyPrefix;
-import com.christian34.easyprefix.files.ConfigData;
 import com.christian34.easyprefix.files.ConfigKeys;
 import com.christian34.easyprefix.groups.Group;
 import com.christian34.easyprefix.messages.Message;
@@ -10,8 +9,6 @@ import com.christian34.easyprefix.user.User;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -40,32 +37,6 @@ public class JoinListener implements Listener {
             Group group = user.getGroup();
             String joinMsg = group.getJoinMessage(user);
             e.setJoinMessage(joinMsg);
-        }
-
-        ConfigData configData = this.instance.getFileManager().getConfig();
-        if (configData.getData().getBoolean("config.join-quit-messages.sound.join.enabled")) {
-            String cfg = configData.getData().getString("config.join-quit-messages.sound.join.sound");
-            if (cfg == null) return;
-            String[] soundOption = cfg.replace(" ", "").split(";");
-            try {
-                Sound sound = Sound.valueOf(soundOption[0]);
-                float volume = Integer.parseInt(soundOption[1]);
-                float pitch = Integer.parseInt(soundOption[2]);
-                if (soundOption.length == 3) {
-                    String receiver = ConfigKeys.JOIN_QUIT_SOUND_RECEIVER.toString();
-                    if (receiver.equals("all")) {
-                        for (Player target : Bukkit.getOnlinePlayers()) {
-                            target.playSound(target.getLocation(), sound, volume, pitch);
-                        }
-                    } else {
-                        user.getPlayer().playSound(user.getPlayer().getLocation(), sound, volume, pitch);
-                    }
-                } else {
-                    Messages.log("&cCouldn't play sound on player join. Please check up the sound configuration.");
-                }
-            } catch (IllegalArgumentException ignored) {
-                Messages.log("&cCouldn't play sound '" + soundOption[0] + "'. Please use valid sounds!");
-            }
         }
     }
 
--- src/main/java/com/christian34/easyprefix/listeners/QuitListener.java
+++ src/main/java/com/christian34/easyprefix/listeners/QuitListener.java
@@ -3,11 +3,7 @@ package com.christian34.easyprefix.listeners;
 import com.christian34.easyprefix.EasyPrefix;
 import com.christian34.easyprefix.files.ConfigKeys;
 import com.christian34.easyprefix.groups.Group;
-import com.christian34.easyprefix.messages.Messages;
 import com.christian34.easyprefix.user.User;
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -40,29 +36,6 @@ public class QuitListener implements Listener {
                 Group group = user.getGroup();
                 String quitMsg = group.getQuitMessage(user);
                 e.setQuitMessage(quitMsg);
-            }
-        }
-        if (ConfigKeys.USE_QUIT_SOUND.toBoolean()) {
-            String cfg = ConfigKeys.QUIT_SOUND.toString();
-            String[] soundOption = cfg.replace(" ", "").split(";");
-            try {
-                Sound sound = Sound.valueOf(soundOption[0]);
-                float volume = Integer.parseInt(soundOption[1]);
-                float pitch = Integer.parseInt(soundOption[2]);
-                if (soundOption.length == 3) {
-                    String receiver = ConfigKeys.JOIN_QUIT_SOUND_RECEIVER.toString();
-                    if (receiver.equals("all")) {
-                        for (Player target : Bukkit.getOnlinePlayers()) {
-                            target.playSound(target.getLocation(), sound, volume, pitch);
-                        }
-                    } else {
-                        user.getPlayer().playSound(user.getPlayer().getLocation(), sound, volume, pitch);
-                    }
-                } else {
-                    Messages.log("&cCouldn't play sound on player quit. Please check up the sound configuration.");
-                }
-            } catch (IllegalArgumentException ignored) {
-                Messages.log("&cCouldn't play sound '" + soundOption[0] + "'. Please use valid sounds!");
             }
         }
         this.instance.unloadUser(e.getPlayer());
