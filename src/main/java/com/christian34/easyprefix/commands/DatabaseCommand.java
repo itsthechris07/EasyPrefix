34c6b0d54de1901e21d4fd82a57182f7c66e985f
chris3105x <christian.haas@outlook.at>
2020-10-14 09:00:44 +0200

    + show help user hasn't entered enough arguments

--- src/main/java/com/christian34/easyprefix/commands/DatabaseCommand.java
+++ src/main/java/com/christian34/easyprefix/commands/DatabaseCommand.java
@@ -40,10 +40,17 @@ public class DatabaseCommand implements Subcommand {
             return;
         }
 
-        if (args.get(1).equalsIgnoreCase("upload")) {
-            sender.sendMessage(Messages.getPrefix() + "§7Uploading data to database. This could take a while...");
-            instance.getDataMigration().upload();
+        if (args.size() == 2) {
+            if (args.get(1).equalsIgnoreCase("upload")) {
+                sender.sendMessage(Messages.getPrefix() + "§7Uploading data to database. This could take a while...");
+                instance.getDataMigration().upload();
+            }
+        } else {
+            sender.sendMessage(" \n§7---------------=== §5§lEasyPrefix §7===---------------\n ");
+            sender.sendMessage("§7/§5EasyPrefix database upload §f| §7upload local data to mysql, this could take a while... (necessary for bungeecord)");
+            sender.sendMessage(" \n§7------------------------------------------------\n ");
         }
+
     }
 
     @Override
