7220503330949b85432b36d1fc49b73fa9ab3eb6
chris <christian.haas37@gmail.com>
2023-08-06 15:13:48 +0200

    / UserDataFile won't be removed before v1.9

--- src/main/java/com/christian34/easyprefix/user/UserDataFile.java
+++ src/main/java/com/christian34/easyprefix/user/UserDataFile.java
@@ -14,7 +14,7 @@ import java.util.UUID;
  * @author Christian34
  */
 @Deprecated
-@ApiStatus.ScheduledForRemoval(inVersion = "1.8")
+@ApiStatus.ScheduledForRemoval(inVersion = "1.9")
 class UserDataFile {
     private final UUID uniqueId;
     private File file;
